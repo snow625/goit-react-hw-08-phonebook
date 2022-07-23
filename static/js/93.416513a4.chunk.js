9d9b3f39990aae905cd7d070793a13b6566fd01f
Dmitriy Narishkin <97532271+snow625@users.noreply.github.com>
2022-07-23 17:31:07 +0300

    Updates

---
+++ static/js/93.416513a4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[93],{3093:function(e,n,o){o.r(n),o.d(n,{default:function(){return c}});var t=o(3504),r=o(3329),c=function(){return(0,r.jsx)("div",{className:"container",children:(0,r.jsxs)("h2",{children:["404 Page not found ",(0,r.jsx)(t.rU,{to:"/",children:"Go Home?"})]})})}}}]);
+//# sourceMappingURL=93.416513a4.chunk.js.map
--- static/js/93.6eebf523.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[93],{3093:function(e,n,o){o.r(n),o.d(n,{default:function(){return c}});var t=o(3504),r=o(184),c=function(){return(0,r.jsx)("div",{className:"container",children:(0,r.jsxs)("h2",{children:["404 Page not found ",(0,r.jsx)(t.rU,{to:"/",children:"Go Home?"})]})})}}}]);
-//# sourceMappingURL=93.6eebf523.chunk.js.map
